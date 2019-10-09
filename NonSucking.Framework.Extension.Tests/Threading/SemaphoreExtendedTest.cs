349c75b2d25435e203f2a07c6827cbee2b519b81
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2019-10-09 17:39:14 +0200

    Add Tests to project
    * Add test to project but dont add a test yet because something :P

---
+++ NonSucking.Framework.Extension.Tests/Threading/SemaphoreExtendedTest.cs
@@ -0,0 +1,15 @@
+﻿using NonSucking.Framework.Extension.Threading;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NonSucking.Framework.Extension.Tests.Threading
+{
+    [TestFixture]
+    [TestOf(typeof(SemaphoreExtended))]
+    public sealed class SemaphoreExtendedTest
+    {
+       
+    }
+}
---
+++ NonSucking.Framework.Extension.Tests/Threading/SemaphoreExtendedTestCases.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NonSucking.Framework.Extension.Tests.Threading
+{
+    public static class SemaphoreExtendedTestCases
+    {
+    }
+}
