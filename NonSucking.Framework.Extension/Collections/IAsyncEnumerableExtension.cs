4829ae554451f3234721702aec3875acca4acfd0
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2019-10-20 23:22:11 +0200

    Add OnEach with awaitable action
    * Add OnEach with awaitable action on IAsyncEnumerableExtension. So this can support async actions and it's not neccessary to break the async chain

--- NonSucking.Framework.Extension/Collections/IAsyncEnumerableExtension.cs
+++ NonSucking.Framework.Extension/Collections/IAsyncEnumerableExtension.cs
@@ -41,6 +41,24 @@ namespace NonSucking.Framework.Extension.Collections
                 yield return item;
             }
         }
+        /// <summary>
+        /// Executes the specified action for each object in the iteration.
+        /// </summary>
+        /// <typeparam name="T">The type of objects to enumerate</typeparam>
+        /// <param name="enumerable">The enumeration for which the action is to be executed.</param>
+        /// <param name="action">The action to be executed for each object</param>
+        /// <returns>An enumerator with object of the <paramref name="enumerable"/></returns>
+        public static async IAsyncEnumerable<T> OnEach<T>(this IAsyncEnumerable<T> enumerable, Func<T, Task> action)
+        {
+            if (enumerable == null)
+                throw new ArgumentNullException(nameof(enumerable));
+
+            await foreach (T item in enumerable)
+            {
+                await action(item);
+                yield return item;
+            }
+        }
 
         /// <summary>
         /// Executes the specified action for each object in the iteration.
@@ -57,6 +75,23 @@ namespace NonSucking.Framework.Extension.Collections
 
             await foreach (T item in enumerable)
                 action(item);
+        }
+        /// <summary>
+        /// Executes the specified action for each object async in the iteration.
+        /// 
+        /// Ends the linq chain.
+        /// </summary>
+        /// <typeparam name="T">The type of objects to enumerate</typeparam>
+        /// <param name="enumerable">The enumeration for which the action is to be executed.</param>
+        /// <param name="action">The action to be executed for each object</param>
+        /// <returns>A task to wait for the operation</returns>
+        public static async Task ForEach<T>(this IAsyncEnumerable<T> enumerable, Func<T, Task> action)
+        {
+            if (enumerable == null)
+                throw new ArgumentNullException(nameof(enumerable));
+
+            await foreach (var item in enumerable)
+                await action(item);
         }
     }
 }
