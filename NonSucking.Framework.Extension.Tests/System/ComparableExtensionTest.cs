eaf0dc5b49ce18c6c974c09a677fb70f9c71bf2b
Daniel Henk <henk.daniel98@gmail.com>
2020-12-13 20:44:01 +0100

    Adds clamp extension
    Clamp will limit a number to be in between a given range

---
+++ NonSucking.Framework.Extension.Tests/System/ComparableExtensionTest.cs
@@ -0,0 +1,34 @@
+﻿using NonSucking.Framework.Extension.System;
+using NUnit.Framework;
+
+namespace NonSucking.Framework.Extension.Tests.System
+{
+    [TestFixture]
+    [TestOf(typeof(ComparableExtension))]
+    public sealed class ComparableExtensionTest
+    {
+        [TestCase(0, 0, 1, 0, Description = "Zero")]
+        [TestCase(20, 0, 100, 20, Description = "Positive value in between")]
+        [TestCase(200, 0, 100, 100, Description = "Positive maximum")]
+        [TestCase(0, 10, 100, 10, Description = "Positive minimum")]
+        [TestCase(-25, -50, -10, -25, Description = "Negative value in between")]
+        [TestCase(-1, -50, -10, -10, Description = "Negative maximum")]
+        [TestCase(-100, -50, -10, -50, Description = "Negative minimum")]
+        public void Clamp_Integer(int value, int min, int max, int result)
+        {
+            Assert.That(value.Clamp(min, max), Is.EqualTo(result));
+        }
+
+        [TestCase(0.0, 0.0, 1.0, 0.0, Description = "Zero")]
+        [TestCase(20.2, 0.0, 100.1, 20.2, Description = "Positive value in between")]
+        [TestCase(200.2, 0.0, 100.1, 100.1, Description = "Positive maximum")]
+        [TestCase(0.0, 10.1, 100.1, 10.1, Description = "Positive minimum")]
+        [TestCase(-25.2, -50.5, -10.1, -25.2, Description = "Negative value in between")]
+        [TestCase(-1.1, -50.5, -10.1, -10.1, Description = "Negative maximum")]
+        [TestCase(-100.1, -50.5, -10.1, -50.5, Description = "Negative minimum")]
+        public void Clamp_Double(double value, double min, double max, double result)
+        {
+            Assert.That(value.Clamp(min, max), Is.EqualTo(result));
+        }
+    }
+}
---
+++ NonSucking.Framework.Extension/System/ComparableExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NonSucking.Framework.Extension.System
+{
+    /// <summary>
+    /// Extends the <see cref="IComparable"/> interface by more functions
+    /// </summary>
+    public static class ComparableExtension
+    {
+        /// <summary>
+        /// Limits a number to be in between a given range
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="val">The number to be limited</param>
+        /// <param name="min">The inclusive minimum value</param>
+        /// <param name="max">The inclusive maximum value</param>
+        /// <returns>The limited number.
+        /// If smaller than <paramref name="min"/> then <paramref name="min"/> will be returned.
+        /// If greater than <paramref name="max"/> then <paramref name="max"/> will be returned.
+        /// </returns>
+        public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T>
+        {
+            if (val.CompareTo(min) < 0) return min;
+            else if (val.CompareTo(max) > 0) return max;
+            else return val;
+        }
+    }
+}
