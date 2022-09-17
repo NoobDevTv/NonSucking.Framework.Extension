61714ebd86c571277abef1ff92423ba21282002f
jvbsl <jvsbl@gmx.de>
2022-09-17 11:17:36 +0200

    Fixed some nullable warnings * Do 'is not null' test in serialization for nullable bool write and if-condition separately * #nullable enable in all generated code

--- NonSucking.Framework.Serialization/Templates/Attribute/NoosonCustomAttribute.cs
+++ NonSucking.Framework.Serialization/Templates/Attribute/NoosonCustomAttribute.cs
@@ -1,14 +1,16 @@
-﻿using System;
+﻿#nullable enable
+
+using System;
 
 namespace NonSucking.Framework.Serialization
 {
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property | AttributeTargets.Interface | AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
     internal class NoosonCustomAttribute : Attribute
     {
-        public Type SerializeImplementationType { get; set; }
-        public Type DeserializeImplementationType { get; set; }
-        public string SerializeMethodName { get; set; }
-        public string DeserializeMethodName { get; set; }
+        public Type? SerializeImplementationType { get; set; }
+        public Type? DeserializeImplementationType { get; set; }
+        public string? SerializeMethodName { get; set; }
+        public string? DeserializeMethodName { get; set; }
 
         public const string deserializeName = "Deserialize";
         public const string serializeName = "Serialize";
