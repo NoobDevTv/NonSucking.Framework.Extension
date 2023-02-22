0a888f647051ae71afaaf2098dc2a346fe4994b3
susch19 <suschi@ist-einmalig.de>
2023-02-22 21:24:44 +0100

    Added simple  recursion detection
    * so that visual studio doesn't crash all the time just because of a prop on some class somewhere

---
+++ DEMO/RecursionTest.cs
@@ -0,0 +1,20 @@
+﻿namespace DEMO;
+
+using NonSucking.Framework.Serialization;
+
+[Nooson]
+public partial class RecursionTest
+{
+    public TestRecursion Recursion { get; set; }
+
+    public class TestRecursion
+    {
+        
+        public TestRecursion2 TestProp { get; set; }
+    }
+    public class TestRecursion2
+    {
+        public TestRecursion TestProp { get; set; }
+    }
+
+}
