07ed0846ef138b5f1436143ae04b09d9dcb465ac
susch19 <suschi@ist-einmalig.de>
2023-03-08 21:34:44 +0100

    Added support for DeserializeSelf method
    * so that instances themselfes can be deserialized, but with a different name, because it would otherwise collide with the static deserialize method
    * fixed apotheker => apothecary german english mix error in warning
    * fixed base not called on some types, because the generation order was not correctly done, we now sort by amount of base members, as this should most of the time result in better or even correct ordering

--- NonSucking.Framework.Serialization/Consts.cs
+++ NonSucking.Framework.Serialization/Consts.cs
@@ -9,6 +9,7 @@ internal static class Consts
     internal const string GenericParameterWriterName = "TNonSuckingWriter";
     internal const string Serialize = nameof(Serialize);
     internal const string Deserialize = nameof(Deserialize);
+    internal const string DeserializeSelf = nameof(DeserializeSelf);
     internal const string InstanceParameterName = "that";
     internal const string ThisName = "this";
 }
